c00e194c14af423b7f89f86825c063399984e672
Daniel Brandenburg <dabr@dhigroup.com>
2024-05-23 16:55:26 +0200

    init commands

---
+++ internal/tui/main.go
@@ -0,0 +1,10 @@
+package tui
+
+import "github.com/rivo/tview"
+
+func Main() {
+	box := tview.NewBox().SetBorder(true).SetTitle("Hello, world!")
+	if err := tview.NewApplication().SetRoot(box, true).Run(); err != nil {
+		panic(err)
+	}
+}
---
+++ main.go
@@ -0,0 +1,10 @@
+package main
+
+import (
+	"github.com/lazyhttp/lazyhttp/cmd"
+)
+
+func main() {
+
+	cmd.Execute()
+}
